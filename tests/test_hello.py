01d8665289603376ec3b5378cf5973fa86145aa7
Amit Hauzer <amit.hauzer@gmail.com>
2025-06-19 21:17:17 +0300

    fix kubernetes error 2

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,10 +1,30 @@
 import requests
 import time
+import pytest
+import subprocess
 
 
 def test_flask_app_responds():
     # Optional wait if the app starts slowly
     time.sleep(1)
     response = requests.get("http://localhost:5000/")
+    assert response.status_code == 200
+    assert response.text.strip() == "Hello, World!"
+
+
+@pytest.mark.kubernetes
+def test_flask_app_responds_via_kubernetes():
+    # This test assumes that the Flask app is running in a Kubernetes cluster
+    # and that the service is accessible via the specified URL.
+    # Replace with the actual service URL if needed.
+
+    NODE_PORT: str = subprocess.check_output(
+        ["kubectl", "get", "--namespace", "default", "-o", 'jsonpath={.spec.ports[0].nodePort}', "services", "test-app-amitchart"], text=True).strip()
+    NODE_IP: str = subprocess.check_output(["kubectl", "get", "nodes", "--namespace", "default",
+                                           "-o", 'jsonpath={.items[0].status.addresses[0].address}'], text=True).strip()
+    URL: str = f"http://{NODE_IP}:{NODE_PORT}/"
+    print(f"Testing Flask app at {URL}")
+
+    response = requests.get(URL)
     assert response.status_code == 200
     assert response.text.strip() == "Hello, World!"
